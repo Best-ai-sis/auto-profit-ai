931b63848976704dda4e4fd6a572f58fd8f432ef
Best-ai-sis <P76502713@gmail.com>
2025-04-23 21:15:45 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
 print('Добро пожаловать в Авто-прибыль ИИ')
+
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Сервер запущен!"
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=10000)
