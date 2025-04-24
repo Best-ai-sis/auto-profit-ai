aee5979bb0f46c68e51fe880943e622b8db19a46
Best-ai-sis <P76502713@gmail.com>
2025-04-24 14:04:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
-print('Добро пожаловать в Авто-прибыль ИИ')
+import telebot
+from flask import Flask, request
 
-from flask import Flask
-
+API_TOKEN = '7729733399:AAGRQH42MQYrl80VdzfCEWXCkafSR4Z8dvA'
+bot = telebot.TeleBot(API_TOKEN)
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return "Сервер запущен!"
+@bot.message_handler(commands=['start'])
+def handle_start(message):
+    bot.send_message(message.chat.id, "Бот запущен. Добро пожаловать!")
+
+@app.route('/', methods=['POST'])
+def webhook():
+    bot.process_new_updates([telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
+
+@app.route('/', methods=['GET'])
+def index():
+    return "Сервер и бот запущены!"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=10000)
