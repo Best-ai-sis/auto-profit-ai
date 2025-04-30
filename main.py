eaa683838f63f14c119d1ac3ae633b515b266141
Best-ai-sis <P76502713@gmail.com>
2025-04-30 10:51:20 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,24 @@
-from telegram.ext import Application, CommandHandler
-from bot_config import API_TOKEN
+import os
+import telebot
+from flask import Flask, request
 
-async def start(update, context):
-    await update.message.reply_text("Бот активен!")
+API_TOKEN = os.getenv("BOT_TOKEN")
+bot = telebot.TeleBot(API_TOKEN)
 
-def main():
-    app = Application.builder().token(API_TOKEN).build()
-    app.add_handler(CommandHandler("start", start))
-    app.run_polling()
+app = Flask(__name__)
 
-if __name__ == '__main__':
-    main()
+@bot.message_handler(commands=['start'])
+def start_message(message):
+    bot.send_message(message.chat.id, "Бот работает.")
+
+@app.route(f"/{API_TOKEN}", methods=["POST"])
+def webhook():
+    json_str = request.get_data().decode('UTF-8')
+    update = telebot.types.Update.de_json(json_str)
+    bot.process_new_updates([update])
+    return "!", 200
+
+if __name__ == "__main__":
+    bot.remove_webhook()
+    bot.set_webhook(url=os.getenv("RENDER_EXTERNAL_URL") + "/" + API_TOKEN)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
